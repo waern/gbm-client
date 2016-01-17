7a9b8c9b82d23e6e9461af143c47d0bd002668e9
David Waern <david.waern@gmail.com>
2016-01-17 22:07:36 +0100

    Hack to make app bundle creation work in pure darwin env

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,13 @@
 import Distribution.Simple
 import Distribution.MacOSX
 
-app = MacApp {appName = "gbm-client", appIcon = Nothing, appPlist = Nothing, resources = [], otherBins = [], appDeps = ChaseWithDefaults}
+exclusions =
+  ["/System/Library/Frameworks/",
+   --"/libSystem.",
+   "/libgcc_s.",
+   "/libobjc."
+  ]
+
+app = MacApp {appName = "gbm-client", appIcon = Nothing, appPlist = Nothing, resources = [], otherBins = [], appDeps = ChaseWith exclusions}
 
 main = defaultMainWithHooks simpleUserHooks {postBuild = appBundleBuildHook [app]}
